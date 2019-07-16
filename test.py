c7b826f726f0cef3673992491bc7535c11632cee
ajitabhkalta <ajitabhkalta@gmail.com>
2019-07-16 11:27:54 +0530

    Day 3 python training

---
+++ test.py
@@ -0,0 +1,13 @@
+num=eval(input("enter a number"))
+if(num>0):
+    print("positive")
+else:
+    print("negative")
+
+
+if(num>0):
+    print("positive")
+elif(num<0):
+    print("negative")
+else:
+    print("Zero value provided")
